7da6ae6ec3b71058c8df12f38b3f6f6a3b269daa
Esteban Echeverry <esteban.e0418@gmail.com>
2020-06-28 12:24:22 -0500

    Implemented table with basic pagination and sorting

--- src/molecules/SearchForm/SearchForm.js
+++ src/molecules/SearchForm/SearchForm.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Button, TextField } from '@material-ui/core';
 import axios from 'axios';
+import MaterialTable from 'material-table';
+
 
 class SearchForm extends React.Component {
     constructor(props){
         super(props);
         this.state = {
+            isUserFound: true,
             search: '',
             repositories: [],
-            isUserFound: true
+            tableData: []
         }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    setTableData(repositories) {
+        return repositories.map( ({language, default_branch, html_url, name, description }) => {
+            return { language, default_branch, html_url, name, description }
+        })
     }
 
     handleChange(e) {
@@ -24,8 +33,12 @@ class SearchForm extends React.Component {
         axios.get(`https://api.github.com/users/${this.state.search}/repos`)
             .then(res => {
                 const repositories = res.data;
-                this.setState({ repositories, isUserFound: true });
-            }, e => this.setState({ isUserFound: false}))
+                this.setState({ 
+                    tableData: this.setTableData(repositories), 
+                    isUserFound: true
+                });
+                
+            }, () => this.setState({ isUserFound: false}))
     }
 
     render() {
@@ -41,10 +54,18 @@ class SearchForm extends React.Component {
                     >
                         Search
                     </Button>
-                    <ul>
-                        {this.state.repositories.map( repo => <li>{repo.name}</li>)}
-                    </ul>
                 </form>
+                <MaterialTable
+                    columns={[
+                        { title: 'Language', field: 'language' },
+                        { title: 'Default Branch', field: 'default_branch' },
+                        { title: 'URL', field: 'html_url' },
+                        { title: 'Name', field: 'name' },
+                        { title: 'Description', field: 'description' }
+                    ]}
+                    data={this.state.tableData}
+                    title="User Repositories"
+                />
                 { !this.state.isUserFound && <p>User has not been found</p>}
             </div>
         );
